edbc80a5ad320ce3a03ab8891a8224c8d1138c03
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-06-29 08:19:00 -0400

    Made some special adjustments to the game.

--- Math Quiz 01 MOO ICT/Form1.Designer.cs
+++ Math Quiz 01 MOO ICT/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -106,12 +107,25 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(74, 57);
             this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.ChequearRespuesta);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(421, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 52);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Comprobar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EventoCliquearBotonChequeo);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 128);
+            this.ClientSize = new System.Drawing.Size(592, 128);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label5);
@@ -135,6 +149,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
