533b9374f70481d4fc5731025252bda4e0e0047c
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-06-30 01:42:41 -0400

    We have made some fixes for the two values ​​that can be randomized by math operations.

--- Math Quiz 01 MOO ICT/Form1.cs
+++ Math Quiz 01 MOO ICT/Form1.cs
@@ -40,14 +40,54 @@ namespace Math_Quiz_01_MOO_ICT
             }
         }
 
+        // Al clickear el botón de chequear respuesta, se muestran las acciones mediante un método para posteriores revisiones de ejecución.
         private void EventoCliquearBotonChequeo(object sender, EventArgs e)
         {
-            // En instantes...
+            SetUpGame(); // Llamado del método anterior.
         }
 
+        // Método para arreglar la interfaz del juego.
         private void SetUpGame()
         {
-            // En instantes...
+            int numA = rnd.Next(10, 20); // Le pasamos valores aleatorios del 10 al 20 en el valor 1.
+            int numB = rnd.Next(0, 9); // Le pasamos valores aleatorios del 0 al 9 en el valor 2.
+
+            txtAnswer.Text = null; // La respuesta debe ser nula, es decir, debe estar asociado al valor ingresado correctamente entre estos dos valores.
+
+            // Lo vamos a categorizar por 3 operaciones fundamentales (Excepto la división).
+
+            switch (Maths[rnd.Next(0, Maths.Length)])
+            {
+                case "Adición": // Suma.
+
+                    total = numA + numB; // Ej: 14 + 2 = 16
+                    lblSymbol.Text = "+"; // Símbolo para sumar 2 valores.
+                    lblSymbol.ForeColor = Color.Red;  // Rojo para la suma.
+
+                    break;
+
+                 case "Sustracción": // Resta.
+
+                    total = numA - numB; // Ej: 14 - 2 = 12
+                    lblSymbol.Text = "-"; // Símbolo para restar 2 valores.
+                    lblSymbol.ForeColor = Color.Blue; // Azul para la resta.
+
+                    break;
+
+                 case "Multiplicación": // Multiplicación.
+
+                    total = numA * numB; // Ej: 8 * 2 = 16
+                    lblSymbol.Text = "x"; // Símbolo para multiplicar 2 valores.
+                    lblSymbol.ForeColor = Color.Yellow; // Amarillo para la multiplicación.
+
+                    break;
+            }
+
+            // Para los 2 valores:
+
+            lblNumA.Text = numA.ToString(); // Valor 1.
+            lblNumB.Text = numB.ToString(); // Valor 2.
+
         }
     }
 }
